fff377989caac0c27743ff0e3137c63ffd0a2800
Ayush Sahu <ayushsahu050502@gmail.com>
2023-07-16 03:58:26 +0530

    gcd
    GREATEST COMMON DIVISOR

---
+++ gcd.cpp
@@ -0,0 +1,29 @@
+//Euclidean Algorithm
+//Basic idea->Let 'b'be smaller than 'a' than [gcd(a,b)==gcd(a-b,b)]
+//Reason->Let 'g' be gcd of'a' , 'b' 
+//a=gx , b=gy , gcd(x,y)=1
+//(a-b)=g(x-y)
+
+// C++ Code iterative
+int gcd(int a,int b)
+{
+    while(a!=b)
+    {
+        if(a>b)
+        a=a-b;
+        else
+        b=b-a;
+    }
+    return a;
+}
+
+// C++ code recursive
+int gcd(int a,int b)
+{
+    if(b==0)
+    return a;
+    else
+    return gcd(b,a%b);
+}
+
+//Day one challenge compleated :)
